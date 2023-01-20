1decbd29d40d4c553ac9b6b455f25fb90eb808ea
Tricker <Tricker@DESKTOP-7IKSFFB>
2023-01-20 22:13:56 +0100

    Add project files.

---
+++ JewelCraft/JewelCraft.cs
@@ -0,0 +1,36 @@
+// JewelCraft
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    JewelCraft.cs
+// Project: JewelCraft
+
+using BepInEx;
+using Jotunn.Entities;
+using Jotunn.Managers;
+
+namespace JewelCraft
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class JewelCraft : BaseUnityPlugin
+    {
+        public const string PluginGUID = "c4478e31-4ee5-456e-80e3-c4e956b5026e";
+        public const string PluginName = "JewelCraft";
+        public const string PluginVersion = "0.0.1";
+        
+        // Use this class to add your own localization to the game
+        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
+        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+
+        private void Awake()
+        {
+            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            Jotunn.Logger.LogInfo("JewelCraft has landed");
+            
+            // To learn more about Jotunn's features, go to
+            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+        }
+    }
+}
+
